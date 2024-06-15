eb191a1ae8e7cd4ddfab2a875247aa223a6288dc
Johanna Fagerås <heyjohfa@gmail.com>
2024-06-15 12:19:52 +0200

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,18 @@
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("public/css");
+    eleventyConfig.addPassthroughCopy("public/js");
+    eleventyConfig.addPassthroughCopy("public/images");
+    eleventyConfig.addPassthroughCopy("public/fonts");
+    eleventyConfig.addPassthroughCopy("public/symbols");
+
+    return {
+      markdownTemplateEngine: 'njk',
+      dataTemplateEngine: 'njk',
+      htmlTemplateEngine: 'njk',
+      dir: {
+        input: "src",
+        includes: "_includes",
+        output: "docs"
+        }
+    };
+};
